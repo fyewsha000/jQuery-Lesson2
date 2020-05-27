afadce5f60d60a30537a910fd826d9215fd8abad
佐々木 優葉 <yuha000@MacBook-Pro.local>
2020-05-28 00:19:33 +0900

    [Start]6-9

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(".theTarget").skipper({
+
+	transition : 'fade',
+
+	speed : 1000,
+
+	easing : 'easeOutQuart',
+
+	navtype : 'block',
+
+	childrenElementType : 'div',
+
+	arrows : true,
+
+	autoPlay : false,
+
+	autoPlayDuration : 5000,
+
+	keyboardOnAlways : true,
+
+	hidePrevious : false,
+});
